a29c2a9d4294201ba968837729465622b2665237
shinsojeong <shinsojeong82@gmail.com>
2021-09-28 20:08:44 +0900

    210928 typescript, webpack 적용

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/index.tsx'
+  ],
+  output: {
+    filename: "bundle.js",
+    publicPath: "/",
+    path: __dirname + "/dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      "/api": "http://localhost:8080"
+    }
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx','.ts','.tsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      templateParameters: {
+        env: process.env.NODE_ENV === 'production' ? '' : '[DEV]',
+      },
+      minify:
+        process.env.NODE_ENV === 'production'
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
+    }),
+  ],
+};
