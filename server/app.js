09c54fbbefb80f8fd07d9b5a107d2e1143749b94
shinsojeong <shinsojeong82@gmail.com>
2023-09-15 12:05:10 +0900

    OCR 오류 처리

--- server/app.js
+++ server/app.js
@@ -79,5 +79,5 @@ app.use((err, req, res, next) => {
 })
 
 app.listen(app.get("port"), () => {
-  console.log(app.get("port"),'번 포트에서 대기 중');
+  console.log("SERVER PORT: ", app.get("port"))
 });
--- src/component/camera/Camera.tsx
+++ src/component/camera/Camera.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import Tesseract from 'tesseract.js';
 
 import { changeTop } from '../../module/bar';
+import Sample from '../../source/sample.jpg';
 import Files_And_Folder_Flatline from '../../source/Files_And_Folder_Flatline.png';
 import '../../style/Camera.scss';
 
@@ -33,39 +34,48 @@ export default function Camera(): ReactElement {
 
   //OCR
   const recognize = (picture: File) => {
-    Tesseract.recognize(
-      picture,
-      'kor'
-    ).then(async({ data: { text } }) => {
-      const trimStr = text.replace(/(\s*)/g, "");
-      const nameStr = trimStr.split('[');
-      const timeNumStr = trimStr.split(']');
-      
-      const res = await axios.post(`${url}/drug/get_drug_info`, {
-        itemName: nameStr[0]
-      }, {
-        withCredentials: true
-      })
-
-      const { header, body } = (JSON.parse(res.data.data)).response;
-
-      if (header.resultCode._text == '00') {
-        navigate(
-          "/user/create-medi-info",
-          {
-            state: {
-              medi_code: body.items.item.itemSeq._text,
-              medi_name: body.items.item.itemName._text,
-              medi_times: timeNumStr[1][0],
-              medi_num: timeNumStr[1][3]
+    try {
+      Tesseract.recognize(
+        picture,
+        'kor'
+      ).then(async({ data: { text } }) => {
+        const trimStr = text.replace(/(\s*)/g, "");
+        const nameStr = trimStr.split('[');
+        const timeNumStr = trimStr.split(']');
+        
+        const res = await axios.post(`${url}/drug/get_drug_info`, {
+          itemName: nameStr[0]
+        }, {
+          withCredentials: true
+        })
+  
+        const { header, body } = (JSON.parse(res.data.data)).response;
+        const medi_code = body.items?.item?.itemSeq._text || "",
+              medi_name = body.items?.item?.itemName._text || "",
+              medi_times = timeNumStr.length > 1 && timeNumStr[1].length > 0 ? timeNumStr[1][0] : 0,
+              medi_num = timeNumStr.length > 1 && timeNumStr[1].length > 3 ? timeNumStr[1][3] : 0
+  
+        if (header.resultCode._text == '00') {
+          if(medi_code === "" || medi_name === "") return alert("지원하지 않는 약봉투 양식입니다.")
+          navigate(
+            "/user/create-medi-info",
+            {
+              state: {
+                medi_code, 
+                medi_name, 
+                medi_times, 
+                medi_num
+              }
             }
-          }
-        );
-      } else {
-        alert("일치하는 결과가 없습니다.");
-      }
-      
-    })
+          );
+        } else {
+          alert("일치하는 결과가 없습니다.");
+        }
+        
+      })
+    } catch(e) {
+      alert("약봉투 인식 중 오류가 발생했습니다.")
+    }
   }
 
   return (
@@ -82,6 +92,10 @@ export default function Camera(): ReactElement {
             uploadImg(e.target.files)
           }}
         ></input>
+        <div id="messages">
+          <p>*현재는 아래 양식의 약봉투만 인식 가능*</p>
+          <img src={Sample} width="90%"/>
+        </div>
       </div>
       <img src={Files_And_Folder_Flatline} width="250px"/>
     </div>
