2dbb068271763968f0244483cc6fd665b6921a9f
shinsojeong <shinsojeong82@gmail.com>
2023-09-17 19:10:39 +0900

    패키지 업데이트에 따른 오류 처리

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from 'react-dom/client';
 
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -9,20 +9,20 @@ import App from './App';
 import persistedReducer from './module/store';
 import { persistStore } from 'redux-persist';
 
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, legacy_createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
 
-const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(ReduxThunk)));
+const store = legacy_createStore(persistedReducer, composeWithDevTools(applyMiddleware(ReduxThunk)));
 const persistor = persistStore(store);
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </PersistGate>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
